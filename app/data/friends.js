10f4dad24596bd0395c73e5ccc8ac5122f95478d
David Luu <David.n.luu@gmail.com>
2018-08-01 17:43:50 -0500

    Everything added, but still working on apiRoute.

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+let friends = [
+    {
+        name: "David L",
+        photo: "stock.img",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Barney S",
+        photo: "stock.img",
+        scores: [
+            3,
+            2,
+            4,
+            1,
+            3,
+            5,
+            5,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Moe S",
+        photo: "stock.img",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+]
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+const friend = require("../data/friends");
+
+module.exports = app => {
+    app.get("/api/friends", (req, res) => {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", (req, res) => {
+        res.json({});
+    })
+}
